e2c6fe6f9dedfe43e45b2682d1dd6496bc5922f3
EnisGjocaj <enisg1489@gmsil.com>
2024-01-08 19:49:56 +0100

    m

--- dashboard/views.py
+++ dashboard/views.py
@@ -33,6 +33,12 @@ from datetime import timedelta
 
 from challenges.models import LabScore
 
+from django.contrib.auth.decorators import user_passes_test
+
+def is_admin(user):
+    return user.is_authenticated and user.is_staff
+
+@user_passes_test(is_admin)
 def index(request):
 
     #code for listing the highest scores START
